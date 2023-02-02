10aaaca1973986904ecf7cdb60a930090a750958
Prince Raj <princefebraj08@gmail.com>
2023-02-02 16:14:49 +0530

    Added: Header product type click

--- js/header.js
+++ js/header.js
@@ -1,80 +1,144 @@
 const headerContent = `    
 <header class="header_style_2">
-    <div class="container-fluid px-0">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container px-0">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse header_nav" id="navbarSupportedContent">
-                <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link  active" aria-current="page" href="index.html">Home</a>
-                  </li>   
-    
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Corporateoverview</a>
-                  </li>
-    
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">marketsegment</a>
-                  </li>
-                  
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" >
-                        products
-                    </a>
-                    <ul class="sub-menu product_sub_menu commn_sub_menu_height px-2 pb-3">
-                        <div class="container">
-                            <div class="micro_menu row mx-0 d-flex">
-                                <div class="micro_menu_li w-auto "  onclick='window.open("./new_products/Flexible_Pipe_System/FlexiblePipeSystem.html");'>
-                                    <li class="clickme micro_menu_li_head">
-                                      <a class=" inner_item">Flexible Pipe System</a></li>
-                                    <li><a href="">Rubber Expansion Bellow </a></li>
-                                    <li><a href="">Stainless steel bellow</a></li>
-                                    <li><a href="">Fabric bellow</a></li>
-                                    <li><a href="">Pneumatic Air Bellow </a></li>
-                                    <li><a href="">PTFE Bellow  </a></li>  
-                                    <li><a href="">Protective bellow  </a></li>
-                                    <li><a href="">Bus Duct Bellow  </a></li>
-                                </div>
-                                <div class="micro_menu_li w-auto" onclick='window.open("./new_products/Vibration_Isolation/Vibration_Isolation.html");'>
-                                    <li class="micro_menu_li_head"><a href="" class="">Vibration Isolation</a></li>                                        
-                                    <li><a href="">Pad  </a></li>
-                                    <li><a href="project-detail.html">Rubber and Metal Bonded anti vibration mounded</a></li>
-                                    <li><a href="project-grid.html">Spring Based Vibration Isolator </a></li>                                        
-                                    <li><a href="project-masonry.html">Riser Isolator Support </a></li>
-                                    <li><a href="project-carousel.html">Wire Rope Suspension System </a></li>
-                                </div>
-                                <div class="micro_menu_li  w-auto" onclick='window.open("./new_products/Mechanical_Seal/Mechanical_Seal.html");'>
-                                    <li class="micro_menu_li_head"><a href="" >Mechanical seal</a></li>                                        
-                                    <li><a href="project-masonry.html">Rubber Bellow Seal </a></li>
-                                    <li><a href="project-carousel.html">Metal Bellow Seal </a></li>
-                                    <li><a href="project-detail.html">Teflon Bellow Seal </a></li>
-                                    <li><a href="project-grid.html">Grundfos Mechanical seal </a></li>
-                                    <li><a href="project-carousel.html">Multi Spring Mechanical Seal  </a></li>
-                                    <li><a href="project-detail.html">Single Spring Mechanical Seal  </a></li>
-                                    <li><a href="project-detail.html">Agitator Seal  </a></li>
-                                    <li><a href="project-detail.html">Cartridge Seal  </a></li>
-                                    <li><a href="project-detail.html">Dry Running Seal  </a></li>
-                                </div>
-                                <div class="micro_menu_li  w-auto" onclick='window.open("./new_products/Flexible_Hose/Flexible_Hose.html");'>
-                                    <li class="micro_menu_li_head"><a href="project-grid.html" >Flexible Hose </a></li>                                        
-                                    <li><a href="">Stainless Steel Hose   </a></li>
-                                    <li><a href="project-carousel.html">Rubber Hose   </a></li>
-                                    <li><a href="project-detail.html">Spiral Hose</a></li>
-                                    <li><a href="project-grid.html">PTFE Hose </a></li>
-                                </div>
-                                <div class="micro_menu_li w-auto" onclick='window.open("./new_products/Customized/Customized.html");'>
-                                    <li class="micro_menu_li_head"><a href="project-grid.html" >Customized</a></li>                                        
-                                    <li><a href="">Piston Seal  </a></li>
-                                    <li><a href="project-carousel.html">O-ring  </a></li>
-                                    <li><a href="project-detail.html">Channel  </a></li>
-                                    <li><a href="project-grid.html">Gasket </a></li>                                        
-                                    <li><a href="project-masonry.html"> Coupling </a></li>
-                                    <li><a href="project-carousel.html">Rubber Sheet  </a></li>
-                                    <li><a href="project-detail.html">Mat</a></li>
-                                </div>
+<div class="container-fluid px-0">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container px-0">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse header_nav" id="navbarSupportedContent">
+            <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link  active" aria-current="page" href="index.html">Home</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Corporateoverview</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">marketsegment</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                    products
+                </a>
+                <ul class="sub-menu product_sub_menu commn_sub_menu_height px-2 pb-3">
+                    <div class="container">
+                        <div class="micro_menu row mx-0 d-flex">
+                            <div class="micro_menu_li w-auto headerCustomMenu" productType="flexiblePipeSystem">
+                                <li class="clickme micro_menu_li_head">
+                                  <span class="inner_item">Flexible Pipe System</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="one">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="one">Rubber Expansion Bellow </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="two">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="two">Stainless steel bellow</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="three">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="three">Fabric bellow</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="four">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="four">Pneumatic Air Bellow </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="five">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="five">PTFE Bellow  </span></
+                                li>  
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="six">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="six">Protective bellow  </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexiblePipeSystem" pageId="seven">
+                                  <span class="headerCustomMenu" productType="flexiblePipeSystem" pageId="seven">Bus Duct Bellow  </span>
+                                </li>
+                            </div>
+                            <div class="micro_menu_li w-auto headerCustomMenu" productType="vibrationIsolation">
+                                <li class="micro_menu_li_head "><span class="">Vibration Isolation</span></li>                                        
+                                <li class="headerCustomMenu" productType="vibrationIsolation" pageId="one">
+                                  <span class="headerCustomMenu" productType="vibrationIsolation" pageId="one">Pad</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="vibrationIsolation" pageId="two">
+                                  <span class="headerCustomMenu" productType="vibrationIsolation" pageId="two">Rubber and Metal Bonded anti vibration mounded</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="vibrationIsolation" pageId="three">
+                                  <span class="headerCustomMenu" productType="vibrationIsolation" pageId="three">Spring Based Vibration Isolator </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="vibrationIsolation" pageId="four">
+                                  <span class="headerCustomMenu" productType="vibrationIsolation" pageId="four">Riser Isolator Support </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="vibrationIsolation" pageId="five">
+                                  <span class="headerCustomMenu" productType="vibrationIsolation" pageId="five">Wire Rope Suspension System </span>
+                                </li>
+                            </div>
+                            <div class="micro_menu_li  w-auto headerCustomMenu" productType="mechanicalSeal">
+                                <li class="micro_menu_li_head "><span class="">Mechanical seal</span></li>                                        
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="one">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="one">Rubber Bellow Seal </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="two">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="two">Metal Bellow Seal </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="three">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="three">Teflon Bellow Seal </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="four">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="four">Grundfos Mechanical seal </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="five">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="five">Multi Spring Mechanical Seal  </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="six">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="six">Single Spring Mechanical Seal  </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="seven">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="seven">Agitator Seal  </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="eight">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="eight">Cartridge Seal  </span>
+                                </li>
+                                <li class="headerCustomMenu" productType="mechanicalSeal" pageId="nine">
+                                  <span class="headerCustomMenu" productType="mechanicalSeal" pageId="nine">Dry Running Seal  </span>
+                                </li>
+                            </div>
+                            <div class="micro_menu_li  w-auto headerCustomMenu" productType="flexibleHose">
+                                <li class="micro_menu_li_head "><span class="">Flexible Hose </span></li>                                        
+                                <li class="headerCustomMenu" productType="flexibleHose" pageId="one">
+                                  <span class="headerCustomMenu" productType="flexibleHose" pageId="one">Stainless Steel Hose</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexibleHose" pageId="two">
+                                  <span class="headerCustomMenu" productType="flexibleHose" pageId="two">Rubber Hose</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexibleHose" pageId="three">
+                                  <span class="headerCustomMenu" productType="flexibleHose" pageId="three">Spiral Hose</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="flexibleHose" pageId="four">
+                                  <span class="headerCustomMenu" productType="flexibleHose" pageId="four">PTFE Hose </span>
+                                </li>
+                            </div>
+                            <div class="micro_menu_li w-auto headerCustomMenu" productType="customized">
+                                <li class="micro_menu_li_head "><span class="" >Customized</span></li>                                        
+                                <li class="headerCustomMenu" productType="customized" pageId="one">
+                                  <span class="headerCustomMenu" productType="customized" pageId="one">Piston Seal</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="customized" pageId="two">
+                                  <span class="headerCustomMenu" productType="customized" pageId="two">O-ring</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="customized" pageId="three">
+                                  <span class="headerCustomMenu" productType="customized" pageId="three">Channel</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="customized" pageId="four">
+                                  <span class="headerCustomMenu" productType="customized" pageId="four">Gasket</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="customized" pageId="five">
+                                  <span class="headerCustomMenu" productType="customized" pageId="five"> Coupling</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="customized" pageId="six">
+                                  <span class="headerCustomMenu" productType="customized" pageId="six">Rubber Sheet</span>
+                                </li>
+                                <li class="headerCustomMenu" productType="customized" pageId="seven">
+                                  <span class="headerCustomMenu" productType="customized" pageId="seven">Mat</span>
+                                </li>
                             </div>
                         </div>
                         
@@ -91,7 +155,52 @@ const headerContent = `
         </nav>
     </div></header>
 `
+const redirect = (productType, id) => {
+  let url = ""
+  switch(productType) {
+    case "flexiblePipeSystem":
+      url = "/new_products/Flexible_Pipe_System/FlexiblePipeSystem.html" + (id ? "?id="+id : "")
+      break;
+    case "vibrationIsolation":
+      url = "/new_products/Vibration_Isolation/Vibration_Isolation.html" + (id ? "?id="+id : "")
+      break;
+    case "mechanicalSeal":
+      url = "/new_products/Mechanical_Seal/Mechanical_Seal.html" + (id ? "?id="+id : "")
+    break;
+    case "flexibleHose":
+      url = "/new_products/Flexible_Hose/Flexible_Hose.html" + (id ? "?id="+id : "")
+    break;
+    case "customized":
+      url = "/new_products/Customized/Customized.html" + (id ? "?id="+id : "")
+    break;
+  }
+  const a = document.createElement('a');
+  a.href = url
+  a.click()
+}
 
-  
-
+// setting header
 document.getElementById("main_header").innerHTML = headerContent
+
+// header click function
+const allHeaderMenus = document.getElementsByClassName("headerCustomMenu")
+for(let i = 0; i<allHeaderMenus?.length; i++){
+  allHeaderMenus[i]?.addEventListener("click", e => {
+    e.stopPropagation();
+    const productType = e.target.getAttribute("productType");
+    const pageId = e.target.getAttribute("pageId");
+    if(productType){
+      redirect(productType, pageId)
+    }
+  })
+}
+
+// getting id and selecting product type
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+const productTypeId = params?.id ? params?.id : ""
+if(productTypeId){
+  displayCard(productTypeId)
+}
