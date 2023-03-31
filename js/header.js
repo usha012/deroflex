41623b41110c9127aa44ce2c1c75f5ce8646d721
Prince Raj <princefebraj08@gmail.com>
2023-03-31 23:17:47 +0530

    Added: products properties

--- js/header.js
+++ js/header.js
@@ -9,7 +9,7 @@ const headerContent = `
           <div class="collapse navbar-collapse header_nav" id="navbarSupportedContent">
             <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link  active" aria-current="page" href="/deroflex">Home</a>
+                <a class="nav-link  active" aria-current="page" href="/">Home</a>
               </li>
 
               <li class="nav-item">
